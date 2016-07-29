0ad3cf44ef6bb32d87104514dc5e64bb8a11002b
Conor O'Brien <conorobrien4god@gmail.com>
2016-07-28 21:21:43 -0400

    fixing some stuff

---
+++ cergos.js
@@ -0,0 +1,9 @@
+var frst = document.getElementById("frst");
+var prev = document.getElementById("prev");
+var rand = document.getElementById("rand");
+var next = document.getElementById("next");
+var last = document.getElementById("last");
+
+frst.addEventListener("click", function(){
+    
+});
